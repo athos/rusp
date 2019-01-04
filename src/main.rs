7c871679e6b5d1e6832df49cf21fbd870dcc860a
Shogo Ohta <athos0220@gmail.com>
2019-01-04 19:23:48 +0900

    Move object definition to dedicated module

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,13 @@
-use std::fmt;
+mod rusp;
 
-#[derive(Debug)]
-enum Object {
-    Nil,
-    T,
-    Number(i32),
-    Symbol(String),
-    Cons { car: Box<Object>, cdr: Box<Object> }
-}
-
-fn write_list(f: &mut fmt::Formatter, obj: &Object) -> fmt::Result {
-    match *obj {
-        Object::Cons { ref car, ref cdr } => {
-            write!(f, "{}", car);
-            match **cdr {
-                Object::Nil => Ok(()),
-                _ => {
-                    write!(f, " ");
-                    write_list(f, cdr)
-                }
-            }
-        },
-        _ => write!(f, ". {}", obj)
-    }
-}
-
-impl fmt::Display for Object {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Object::Nil => write!(f, "nil"),
-            Object::T => write!(f, "t"),
-            Object::Number(ref num) => write!(f,"{}", num),
-            Object::Symbol(ref sym) => write!(f, "{}", sym),
-            Object::Cons { .. } => {
-                write!(f, "(");
-                write_list(f, self);
-                write!(f, ")")
-            }
-        }
-    }
-}
-
-fn symbol(name: &str) -> Object {
-    Object::Symbol(name.to_string())
-}
-
-fn cons(car: Object, cdr: Object) -> Object {
-    Object::Cons { car: Box::new(car), cdr: Box::new(cdr) }
-}
+use rusp::object::{self, Object};
 
 fn main() {
-    let obj = cons(
-        symbol("+"),
-        cons(
+    let obj = object::cons(
+        object::symbol("+"),
+        object::cons(
             Object::Number(1),
-            cons(Object::Number(2), Object::Nil)
+            object::cons(Object::Number(2), Object::Nil)
         )
     );
     println!("{}", obj);
---
+++ src/rusp/object.rs
@@ -0,0 +1,50 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub enum Object {
+    Nil,
+    T,
+    Number(i32),
+    Symbol(String),
+    Cons { car: Box<Object>, cdr: Box<Object> }
+}
+
+fn write_list(f: &mut fmt::Formatter, obj: &Object) -> fmt::Result {
+    match *obj {
+        Object::Cons { ref car, ref cdr } => {
+            write!(f, "{}", car);
+            match **cdr {
+                Object::Nil => Ok(()),
+                _ => {
+                    write!(f, " ");
+                    write_list(f, cdr)
+                }
+            }
+        },
+        _ => write!(f, ". {}", obj)
+    }
+}
+
+impl fmt::Display for Object {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Object::Nil => write!(f, "nil"),
+            Object::T => write!(f, "t"),
+            Object::Number(ref num) => write!(f,"{}", num),
+            Object::Symbol(ref sym) => write!(f, "{}", sym),
+            Object::Cons { .. } => {
+                write!(f, "(");
+                write_list(f, self);
+                write!(f, ")")
+            }
+        }
+    }
+}
+
+pub fn symbol(name: &str) -> Object {
+    Object::Symbol(name.to_string())
+}
+
+pub fn cons(car: Object, cdr: Object) -> Object {
+    Object::Cons { car: Box::new(car), cdr: Box::new(cdr) }
+}
