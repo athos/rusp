ed69ba8c4d79eab14fc1259855691eaa97575488
OHTA Shogo <athos0220@gmail.com>
2019-02-15 22:25:37 +0900

    Cook up simple REPL

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate rusp;
 
 use std::io::{self, BufRead, Write};
+use rusp::compiler as comp;
 use rusp::reader;
+use rusp::vm::Vm;
 
 fn prompt() -> io::Result<()> {
     print!("> ");
@@ -14,8 +16,11 @@ fn main() -> io::Result<()> {
     prompt()?;
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
-        if let Some(obj) = reader::read_string(&line?) {
-            print!("{:?}\n", obj);
+        if let Some(expr) = reader::read_string(&line?) {
+            let code = comp::compile(&expr).unwrap();
+            let mut vm = Vm::new(code);
+            let v = vm.run().unwrap();
+            println!("{}", *v);
         }
         prompt()?;
     }
